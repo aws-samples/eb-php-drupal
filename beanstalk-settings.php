275da07501872fa57aa7ef6a2ef66b643bbabe3d
Michael Wunderlich <mwunderl@amazon.com>
2016-11-06 19:31:53 -0800

    configuration files

---
+++ beanstalk-settings.php
@@ -0,0 +1,23 @@
+<?php
+$databases = array();
+
+$config_directories = array();
+
+$settings['hash_salt'] = $_SERVER['HASH_SALT'];
+
+$settings['update_free_access'] = FALSE;
+
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+$databases['default']['default'] = array (
+  'database' => $_SERVER['RDS_DB_NAME'],
+  'username' => $_SERVER['RDS_USERNAME'],
+  'password' => $_SERVER['RDS_PASSWORD'],
+  'prefix' => '',
+  'host' => $_SERVER['RDS_HOSTNAME'],
+  'port' => $_SERVER['RDS_PORT'],
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = $_SERVER['SYNC_DIR'];
