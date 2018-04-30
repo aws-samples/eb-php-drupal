d1b6a265e7dbcfc6e6667de09c21147d84de80b1
Michael Wunderlich <mwunderl@amazon.com>
2018-04-30 16:35:57 -0700

    update settings file for 8.5

--- beanstalk-settings.php
+++ beanstalk-settings.php
@@ -1,6 +1,8 @@
 <?php
 
- $databases = array();
+// @codingStandardsIgnoreFile
+
+$databases = array();
 
 $config_directories = array();
 
@@ -8,12 +10,23 @@ $settings['hash_salt'] = $_SERVER['HASH_SALT'];
 
 $settings['update_free_access'] = FALSE;
 
-$settings['container_yamls'][] = __DIR__ . '/services.yml';
+if ($settings['hash_salt']) {
+  $prefix = 'drupal.' . hash('sha256', 'drupal.' . $settings['hash_salt']);
+  $apc_loader = new \Symfony\Component\ClassLoader\ApcClassLoader($prefix, $class_loader);
+  unset($prefix);
+  $class_loader->unregister();
+   $apc_loader->register();
+  $class_loader = $apc_loader;
+}
+
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 
 $settings['file_scan_ignore_directories'] = [
   'node_modules',
   'bower_components',
 ];
+
+$settings['entity_update_batch_size'] = 50;
 
 $databases['default']['default'] = array (
   'database' => $_SERVER['RDS_DB_NAME'],
